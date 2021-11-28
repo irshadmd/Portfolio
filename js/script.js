c316bc115f34622f3c9c176b31603c879ba4efbb
irshadmd <irshadmd039@gmail.com>
2021-11-29 01:19:43 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,8 @@
+AOS.init();
+function validate(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+    alert(`Name: ${name} \nEmail: ${email}\nMessage: ${message}`);
+
+}
